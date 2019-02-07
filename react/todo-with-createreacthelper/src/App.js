0b8c3614d1c603ef79d9073710e49adbf6d4e682
Alla <alla@perceptiveautomata.com>
2019-02-07 17:52:14 -0500

    Deleted outmoded code

--- react/todo-with-createreacthelper/src/App.js
+++ react/todo-with-createreacthelper/src/App.js
@@ -1,48 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
-import TodoList2 from './TodoList2'
-import TodoItems2 from './TodoItems2'
 import Todo from './Todo'
 import TodoList from './TodoList'
 
 class App extends Component {
-//  constructor() {
-//    super()
-//    this.state = {
-//      items: [],
-//      currentItem: {text:'', key:''},
-//    }
-//  }
-//  handleInput = e => {
-//    const itemText = e.target.value
-//    const currentItem = { text: itemText, key: Date.now() }
-//    this.setState({
-//      currentItem,
-//    })
-//  }
-//  addItem = e => {
-//    e.preventDefault()
-//    const newItem = this.state.currentItem
-//    if (newItem.text !== '') {
-//      console.log(newItem)
-//      const items = [...this.state.items, newItem]
-//      this.setState({
-//        items: items,
-//        currentItem: { text: '', key: '' },
-//      })
-//    }
-//  }
-//
-//  deleteItem = key => {
-//    const filteredItems = this.state.items.filter(item => {
-//      return item.key !== key
-//    })
-//    this.setState({
-//      items: filteredItems,
-//    })
-//  }
-//
-//  inputElement =React.createRef()
 
     constructor() {
     super()
