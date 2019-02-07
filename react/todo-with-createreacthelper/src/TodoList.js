8629a1d0d4c4e2f73ea657d18875c98dede6359a
Alla <alla@perceptiveautomata.com>
2019-02-07 17:38:52 -0500

    I actually got this to add todos and be structured in a way that makes sense!
    Old code still hanging out but commented tho

--- react/todo-with-createreacthelper/src/TodoList.js
+++ react/todo-with-createreacthelper/src/TodoList.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react'
+import Todo from './Todo'
 
 class TodoList extends Component {
-  componentDidUpdate() {
-    this.props.inputElement.current.focus()
+   constructor(props) {
+    super(props)
+    this.state = {
+      items: props.todos,
+      currentItem: {text:'', key:''},
+    }
   }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      console.log(newItem)
+      let newTodo={id: 5, title: newItem.text}
+      const items = [...this.state.items, newTodo]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: Date.now() },
+
+      })
+    }
+  }
+
+
   render() {
     return (
       <div className="todoListMain">
-        <div className="header">
-          <form onSubmit={this.props.addItem}>
+       <div className="header">
+          <form onSubmit={this.addItem}>
             <input
               placeholder="Task"
-              ref={this.props.inputElement}
-              value={this.props.currentItem.text}
-              onChange={this.props.handleInput}
+              onChange={this.handleInput}
             />
             <button type="submit"> Add Task </button>
           </form>
         </div>
+
+        {this.state.items.map(t => <Todo key={t.id} title={t.title} />)}
       </div>
     )
   }
